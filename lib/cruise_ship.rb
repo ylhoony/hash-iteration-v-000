d853cd73badc387f1b7c0f7a8d859e8f9dd26337
Hoon Lee <yl.hoony@gmail.com>
2017-11-07 09:38:01 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,14 +1,14 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.find do |key, value|
-    
+  passengers.select do |key, value|
+    key == suite_a && value.chr == "A"
   end
 end
