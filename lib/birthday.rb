a4836fc72345635b8e456379574af9e5b1d9f896
Hoon Lee <yl.hoony@gmail.com>
2017-11-07 09:23:58 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,10 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  
 end
-
-
